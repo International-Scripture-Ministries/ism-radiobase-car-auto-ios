86b1890fcac538180ea9af49bd269f272c577379
Kamran Faiz <kamran@apollo.inc>
2025-05-24 22:35:23 +0500

    update

--- dist/esm/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { CarplayCapacitorPlugin } from './definitions';
-declare const CarplayCapacitor: CarplayCapacitorPlugin;
-export * from './definitions';
-export { CarplayCapacitor };
--- dist/esm/index.js
+++
@@ -1,7 +0,0 @@
-import { registerPlugin } from '@capacitor/core';
-const CarplayCapacitor = registerPlugin('CarplayCapacitor', {
-    web: () => import('./web').then(m => new m.CarplayCapacitorWeb()),
-});
-export * from './definitions';
-export { CarplayCapacitor };
-//# sourceMappingURL=index.js.map
